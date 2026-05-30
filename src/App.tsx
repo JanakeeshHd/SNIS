3c310c3126bc5ad68abb7bd8b9ab9a11b3ec4385
Janakeesh Hd <hegdejanakish2004@gmail.com>
2026-05-31 02:52:25 +0530

    fix vercel deploy error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ScrollToTop } from "./components/layout/ScrollToTop";
 import Home from "./pages/Home";
 import About from "./pages/About";
